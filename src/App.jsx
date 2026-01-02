7e23163c0d5b8b7acd93bb4fc3df6dad3094a604
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-02 17:53:56 +0530

    added functionality, when clicking on button the random emoji and it's info will be fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
+import axios from "axios"
 import { useState } from 'react'
+import Card from './components/Card'
 
 const App = () => {
 
-const [count, setCount] = useState(0)
+  const [emoji, setEmoji] = useState({name:'', category:'', group:'',htmlCode:''})
+
+  let getEmoji = async () => {
+    let response = await axios.get("https://emojihub.yurace.pro/api/random")
+    console.log(response.data)
+    let newObj = {name : response.data.name,
+      category : response.data.category,
+      group: response.data.group,
+      htmlCode : response.data.htmlCode[0]
+    }
+    setEmoji(newObj)
+  }
 
   return (
-    <div className='w-screen h-screen p-10'>
-      <h1 className='text-9xl'>{count}</h1>
-      <button onClick={()=>{
-        setCount(count+1);
-      }} className='px-4 py-2 bg-emerald-500 rounded-xl font-bold text-white text-3xl active:scale-90'>Increase</button>
+    <div className='w-screen h-screen p-5 bg-[#3a3a3a]'>
+      <button onClick={getEmoji} className='text-3xl px-4 py-2 text-white bg-[#135a06] active:scale-95 border-2 border-[#bebebe]'>Surprise Me</button>
+      <h1 className='text-9xl'>
+        <span
+          dangerouslySetInnerHTML={{
+            __html: emoji.htmlCode
+          }}
+        />
+      </h1>
+      <h2 className='text-4xl text-white'>Name: {emoji.name}</h2>
+      <h2 className='text-4xl text-white'>Group: {emoji.group}</h2>
+      <h2 className='text-4xl text-white'>Category: {emoji.category}</h2>
+      {/* <Card/> */}
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-60 h-70 bg-red-500'>
+        <img src="https://i.pinimg.com/1200x/22/3e/96/223e96dcf86a1db948fd1b84ec69bcac.jpg" alt="" />
+        <h1 className='text-8xl absolute top-100 left-18'>&#127747;</h1>
+    </div>
+  )
+}
+
+export default Card
