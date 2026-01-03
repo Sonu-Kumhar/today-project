239aef132c554d53bd4168f217cea0b059b5ab69
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-03 11:37:58 +0530

    added the dragonball api and card

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,60 @@
 import React from 'react'
 import axios from "axios"
-import { useState } from 'react'
 import Card from './components/Card'
+import { useState } from 'react'
 
 const App = () => {
 
-  const [emoji, setEmoji] = useState({name:'', category:'', group:'',htmlCode:''})
+  const [allDragons, setAllDragons] = useState([])
+  const [dragonCount, setDragonCount] = useState("")
+  const [display1, setDisplay1] = useState("block")
 
-  let getEmoji = async () => {
-    let response = await axios.get("https://emojihub.yurace.pro/api/random")
-    console.log(response.data)
-    let newObj = {name : response.data.name,
-      category : response.data.category,
-      group: response.data.group,
-      htmlCode : response.data.htmlCode[0]
-    }
-    setEmoji(newObj)
+  let getDragons = async (e) => {
+    e.preventDefault();
+    setDragonCount(dragonCount);
+    let response = await axios.get(`https://dragonball-api.com/api/characters?limit=${dragonCount}&page=1`)
+    let newAllDragons = [...response.data.items];
+    setAllDragons(newAllDragons)
+    setDragonCount("");
+    console.log(dragonCount)
+    setDisplay1("hidden")
+    console.log(response.data.items)
   }
 
   return (
-    <div className='w-screen h-screen p-5 bg-[#3a3a3a]'>
-      <button onClick={getEmoji} className='text-3xl px-4 py-2 text-white bg-[#135a06] active:scale-95 border-2 border-[#bebebe]'>Surprise Me</button>
-      <h1 className='text-9xl'>
-        <span
-          dangerouslySetInnerHTML={{
-            __html: emoji.htmlCode
+    <div className='mainContainer w-full h-screen p-6 px-20'>
+      <form onSubmit={getDragons}>
+        <input
+          type="number"
+          min={1}
+          placeholder='Enter Dragons Count'
+          className='bg-[#535353] text-3xl font-normal text-[#ffffff] border-4 border-[#d1d1d1] px-4 py-2 mr-5'
+          value={dragonCount}
+          required
+          onChange={(e) => {
+            setDragonCount(e.target.value)
           }}
         />
-      </h1>
-      <h2 className='text-4xl text-white'>Name: {emoji.name}</h2>
-      <h2 className='text-4xl text-white'>Group: {emoji.group}</h2>
-      <h2 className='text-4xl text-white'>Category: {emoji.category}</h2>
-      {/* <Card/> */}
+
+        <button className='bg-[#535353] text-3xl font-normal text-[#f1f1f1] border-4 border-[#d1d1d1] px-4 py-2 active:scale-95 mb-10'>Launch Dragons</button>
+      </form>
+
+      <div className='w-full flex gap-15 flex-wrap'>
+        <img className={`${display1} invert absolute top-30 h-80 animate-marquee`} src="https://i.pinimg.com/originals/f9/2d/eb/f92debea10b3bd06e52ecb96695f56a0.gif" alt="" />
+        <img className={`${display1} grayscale absolute top-80 h-120 animate-marquee `} src="https://i.pinimg.com/originals/f9/2d/eb/f92debea10b3bd06e52ecb96695f56a0.gif" alt="" />
+        <img className={`${display1} absolute top-40 left-140 h-100 rotate-90 animate-marquee-opposite`} src="https://i.pinimg.com/originals/f9/2d/eb/f92debea10b3bd06e52ecb96695f56a0.gif" alt="" />
+        {/* <img className={`${display1} absolute top-80 animate-marquee`} src="https://i.pinimg.com/originals/f9/2d/eb/f92debea10b3bd06e52ecb96695f56a0.gif" alt="" /> */}
+
+        
+        {
+          allDragons.map((elem, index) => {
+            return <div key={index}>
+              <Card name={elem.name} affiliation={elem.affiliation} gender={elem.gender} race={elem.race} ki={elem.ki} image={elem.image} />
+            </div>
+          })
+        }
+
+      </div>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,58 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Card = () => {
-  return (
-    <div className='w-60 h-70 bg-red-500'>
-        <img src="https://i.pinimg.com/1200x/22/3e/96/223e96dcf86a1db948fd1b84ec69bcac.jpg" alt="" />
-        <h1 className='text-8xl absolute top-100 left-18'>&#127747;</h1>
-    </div>
-  )
+const Card = ({ name, affiliation, gender, race, ki, image }) => {
+
+    const [coordinateX, setCoordinateX] = useState(0)
+    const [coordinateY, setCoordinateY] = useState(0)
+    const [displayStars, setDisplayStars] = useState("hidden")
+
+    let cardUrl = ["https://i.pinimg.com/736x/18/47/69/18476926ea4f05176521d48ef7dd95ad.jpg",
+        "https://i.pinimg.com/736x/d6/7e/7f/d67e7f219701eb3c3ec729cd28935fa2.jpg",
+        "https://i.pinimg.com/736x/2a/8d/d4/2a8dd41fd6dd0717627c3da7e04ea3d6.jpg",
+        "https://i.pinimg.com/736x/6b/7a/8a/6b7a8a3a93c230a169f09af244c6d25f.jpg",
+    ]
+
+    setTimeout(() => {
+        setDisplayStars("hidden")
+    }, 10000);
+
+    let doSomething = (e) => {
+        console.log(e)
+        console.log(e.clientX, e.clientY);
+        setDisplayStars("block")
+        setCoordinateX(e.clientX)
+        setCoordinateY(e.clientY)
+        console.log("doing something")
+    }
+
+    let randomNum = Math.floor(Math.random()*4);
+    return (
+        <div className='card w-70 h-105 rounded-lg'>
+            <div>
+                <img style={{top:`${coordinateY}px`, left:`${coordinateX}px`}} className={`${displayStars} absolute z-50`} src="https://condaluna.com/assets/stickers/sparkle-trail-1.gif" alt="" />
+                <img style={{top:`${coordinateY+20}px`, left:`${coordinateX+40}px`}} className={`${displayStars} absolute z-50`} src="https://condaluna.com/assets/stickers/sparkle-trail.gif" alt="" />
+            </div>
+            <div className=' relative w-full h-full  [perspective:1000px]'>
+
+                <div className='card-frame w-full h-full transition-transform duration-500 [transform-style:preserve-3d]'>
+                    <img className='object-cover relative dragon-frame' src={cardUrl[randomNum]} alt="" />
+
+                    <div className='w-28'>
+                        <img className='h-80 w-45 absolute image top-20 left-28 object-contain object-center transition-all duration-[500ms]' src={image} alt="" />
+                    </div>
+                    <div className='absolute top-35 left-12 text-3xl px-2 font-semibold'>{name}</div>
+                    <div className='absolute top-46 left-12 text-2xl px-2 font-normal'>{affiliation}</div>
+                    <div className='absolute top-52 left-12 text-2xl px-2 font-normal'>{race}</div>
+                    <div className='absolute top-58 left-12 text-2xl px-2 font-normal'>{gender}</div>
+                    <div className='absolute top-64 left-12 text-2xl px-2 font-normal'>{ki}</div>
+                    <button onClick={doSomething} className='absolute top-78 left-15 text-4xl px-2 text-white py-1 rounded-2xl active:scale-90 cursor-pointer'>Play</button>
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Card
